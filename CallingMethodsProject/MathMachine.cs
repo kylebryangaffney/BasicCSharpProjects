95fc569a2c32b7377be3201b2f079eae69dab1df
Kyle Gaffney <135358743+kylebryangaffney@users.noreply.github.com>
2026-05-06 12:29:48 -1000

    Update for step 288

---
+++ CallingMethodsProject/MathMachine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsProject
+{
+    // class to perform basic math functions recieving and returning an int value
+    public class MathMachine
+    {
+        public int SubtractFive(int x)
+        {
+            return x - 5;
+        }
+        public int AddFive(int x)
+        {
+            return x + 5;
+        }
+
+        public int MultiplyFive(int x)
+        {
+            return x * 5;
+        }
+
+    }
+}
---
+++ CallingMethodsProject/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using CallingMethodsProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // initialize variables 
+            MathMachine mathMachine = new MathMachine();
+            bool isValid = false;
+            int userNumber = 0;
+            int result = 0;
+
+            // get user input
+            Console.WriteLine("Please enter a whole number you would like to perform math functions on: ");
+            string userInput = Console.ReadLine();
+
+            // check if user input is a valid int
+            isValid = int.TryParse(userInput, out userNumber);
+            while (!isValid)
+            {
+                Console.WriteLine("Please enter a valid whole number as a digit ");
+                userInput = Console.ReadLine();
+                isValid = int.TryParse(userInput, out userNumber);
+            }
+
+            // using the math machine to subtract, add, and multiply the user input value, then printing the result
+            result = mathMachine.SubtractFive(userNumber);
+            Console.WriteLine(userNumber + " - 5 = " + result);
+
+            result = mathMachine.AddFive(userNumber);
+            Console.WriteLine(userNumber + " + 5 = " + result);
+
+            result = mathMachine.MultiplyFive(userNumber);
+            Console.WriteLine(userNumber + " * 5 = " + result);
+        }
+    }
+}
