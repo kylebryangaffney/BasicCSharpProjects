407faecbcd4deab60df46aa17c448945cc12491a
Kyle Gaffney <135358743+kylebryangaffney@users.noreply.github.com>
2026-05-06 14:57:36 -1000

    Update for Step 294

---
+++ MethodClassProject/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// helper class to add 1 to a value and modify it, then print out the second value
+// I chose to pass the first value in by reference in order to actually do anything to the first value AND maintain a void function. 
+// I normally would have returned an int value instead of passing by reference
+namespace MethodClassProject
+{
+    public class Helper
+    {
+        public void AddShow(ref int x, int y )
+        {
+            x = x + 1;
+            Console.WriteLine("The second parameter is: " + y);
+        }
+    }
+}
---
+++ MethodClassProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using MethodClassProject;
+using System;
+using System.Collections.Generic;
+
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Helper helper = new Helper();
+
+            int firstValue = 15;
+            int secondValue = 16;
+
+            // update firstValue to 16, and print out the second value
+            helper.AddShow(ref firstValue, secondValue);
+
+            // further update firstValue to 17, and then print out the second value again
+            helper.AddShow(x: ref firstValue, y: secondValue);
+   
+        }
+
+    }
+}
