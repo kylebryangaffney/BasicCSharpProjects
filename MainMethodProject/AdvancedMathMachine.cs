a85fe3ebd268dc9ceb5367d066028d78a6032b57
Kyle Gaffney <135358743+kylebryangaffney@users.noreply.github.com>
2026-05-06 14:08:23 -1000

    Update for Step 292

---
+++ MainMethodProject/AdvancedMathMachine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+// basic class to add 5 to different data types. all of which return an int value
+namespace MainMethodProject
+{
+    public class AdvancedMathMachine
+    {
+        public int AddFive(int x)
+        {
+            return x + 5;
+        }
+
+        public int AddFive(double x)
+        {
+            int y = Convert.ToInt32(x + 5);
+            return y;
+        }
+        public int AddFive(string x)
+        {
+            int y = Convert.ToInt32(x);
+            return y + 5;
+        }
+
+
+    }
+}
---
+++ MainMethodProject/Program.cs
@@ -0,0 +1,72 @@
+﻿using MainMethodProject;
+using System;
+using System.Collections.Generic;
+
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // initialize variables 
+            AdvancedMathMachine mathMachine = new AdvancedMathMachine();
+            int result = 0;
+            
+            bool isValid = false;
+
+            // get user input for int overload
+            Console.WriteLine("Please enter a whole number you would like to perform addition functions on: ");
+            string userInput = Console.ReadLine();
+
+            // check if user input is a valid int
+            int userNumber = 0;
+            isValid = int.TryParse(userInput, out userNumber);
+            while (!isValid)
+            {
+                Console.WriteLine("Please enter a valid whole number as a digit ");
+                userInput = Console.ReadLine();
+                isValid = int.TryParse(userInput, out userNumber);
+            }
+
+            // using the math machine to add then printing the result
+            result = mathMachine.AddFive(userNumber);
+            Console.WriteLine(userNumber + " + 5 = " + result);
+
+            // block for double overload
+
+            Console.WriteLine("Please enter a decimal number you would like to perform addition on: ");
+            userInput = Console.ReadLine();
+
+            // check if user input is a valid int
+            double userDouble = 0.0;
+            isValid = double.TryParse(userInput, out userDouble);
+            while (!isValid)
+            {
+                Console.WriteLine("Please enter a valid decimal number");
+                userInput = Console.ReadLine();
+                isValid = double.TryParse(userInput, out userDouble);
+            }
+
+            // using the math machine to add then printing the result
+            result = mathMachine.AddFive(userDouble);
+            Console.WriteLine(userDouble + " + 5 = " + result);
+
+            // block for string overload 
+            Console.WriteLine("Please enter another whole number you would like to perform addition on: ");
+            userInput = Console.ReadLine();
+
+            // using the math machine to add then printing the result
+            try
+            {
+                result = mathMachine.AddFive(userInput);
+                Console.WriteLine(userInput + " + 5 = " + result);
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine(ex.Message); 
+            }
+
+        }
+    }
+}
