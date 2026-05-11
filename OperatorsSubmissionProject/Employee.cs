7e3304cb9e922613d069db070b128d47dfa07972
Kyle Gaffney <135358743+kylebryangaffney@users.noreply.github.com>
2026-05-11 13:17:54 -1000

    Update for step 327

---
+++ OperatorsSubmissionProject/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmissionProject
+{
+    internal class Employee
+    {
+        // basic information for employee
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+        // simple method to print out the attributes of an employee
+        public void PrintName()
+        {
+            Console.WriteLine("Employee: " + Id);
+            Console.WriteLine(LastName + ", " + FirstName);
+        }
+
+        // override the equality operator to check if the employee's ID numbers are equal.
+        // Must return bool because equality comparison operator returns bool
+        public static bool operator == (Employee emp1, Employee emp2)
+        {
+            return emp1.Id == emp2.Id;
+        }
+
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+
+            return emp1.Id == emp2.Id;
+        }
+    }
+}
---
+++ OperatorsSubmissionProject/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace OperatorsSubmissionProject
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            // instantiate 2 employees to compare
+            Employee johnColtrane = new Employee() { FirstName = "John", LastName = "Coltrane", Id = 1 };
+            Employee dexterGordon = new Employee() { FirstName = "Dexter", LastName = "Gordon", Id = 1 };
+
+            // compare the ID numbers
+            CompareEmployeeId(dexterGordon, johnColtrane);
+
+            // update one of the id numbers and compare again.
+            dexterGordon.Id = 2;
+
+            CompareEmployeeId(dexterGordon, johnColtrane);
+
+        }
+
+        // method to compare employees' ID numbers, use static so that we do not need to make a program instance
+        static void CompareEmployeeId(Employee emp1,  Employee emp2)
+        {
+            emp1.PrintName();
+            emp2.PrintName();
+            if (emp1 == emp2)
+            {
+
+                Console.WriteLine("Both employees have the same employee Id Number");
+            }
+            else
+            {
+                Console.WriteLine("The employees have different Id Numbers");
+            }
+        }
+    }
+}
