d1a60f4caa988e8d21c5b4f84db9037d0f19fe18
Kyle Gaffney <135358743+kylebryangaffney@users.noreply.github.com>
2026-05-11 14:30:18 -1000

    Update for step 331

---
+++ ParametersAssignment/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    // define employee class with different type for the things list
+    internal class Employee<T>
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        int ID { get; set; }
+
+        // using generic to define the data type a particular instance of an employee can use in their list
+        public List<T> Things;
+        // constructor for instantiating an object
+        public Employee(string firstName, string lastName, int id) 
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            ID = id; 
+        }
+        // method to iterate through the list of things, and print them out to the console
+        public void PrintThings()
+        {
+            Console.WriteLine("All of the things possessed by " + FirstName + " " + LastName + " include: ");
+            foreach (T t in Things)
+            {
+                Console.WriteLine(t);
+            }  
+            Console.WriteLine("\n");
+        }
+        
+    }
+}
---
+++ ParametersAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // making a string based employee and setting the list of elements in the Things list
+            Employee<string> stringEmployee = new Employee<string>("John", "Coltrane", 1)
+            {
+                Things = new List<string> { "saxophone", "sheet music", "body", "and soul" }
+            };
+
+            // instantiating an int employee and setting the list of int elements in the things list
+            Employee<int> intEmployee = new Employee<int>("Dexter", "Gordon", 2)
+            {
+                Things = new List<int> { 6, 12, 18, 24 }
+            };
+
+            // call employee method to display Things list from both employee
+            stringEmployee.PrintThings();
+            intEmployee.PrintThings();
+        }
+
+    }
+}
