772c2f60e24b2222e0b5cf80a82957aa9a2d36ac
Kyle Gaffney <135358743+kylebryangaffney@users.noreply.github.com>
2026-05-14 10:09:51 -1000

    Update for step 345

---
+++ StructAssignmentProject/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+
+namespace StructAssignmentProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate a number struct 
+            Number sixtyTwo = new Number(62);
+            // get the correct attribute of the Number struct, and then print it to the console
+            Console.WriteLine(sixtyTwo.Amount.ToString());
+        }
+
+
+    }
+
+    // define the struct
+    struct Number
+    {
+        // public constructor to simplify the process of actually using the struct
+        public Number(decimal value)
+        {
+            Amount = value;
+        }
+        // single attribute for storing the desired value
+        public decimal Amount { get; set; }
+
+    }
+}
